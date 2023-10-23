a84c2570aa8bab5f0de9dd08793a6c17b2a4d237
Ally Gilbert <allymgilbert@gmail.com>
2023-10-23 18:03:55 +0100

    filepath changes

--- filepath.py
+++ filepath.py
@@ -1,3 +1,4 @@
 #This code is to generate a filepath variable to use in "Social Media Mental Health Notebook"
+#Copy paste filepath into notebook when starting, delete when done.
 
 filepath = "/Users/alexandragilbert/Desktop/python_projects/Social Media Mental Health/smmh_project/smmh.csv"
