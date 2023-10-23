b9bb4d19a3cab20e3b48d58967638c0607cc1884
Ally Gilbert <allymgilbert@gmail.com>
2023-10-23 17:52:19 +0100

    new filepath function

---
+++ filepath.py
@@ -0,0 +1,3 @@
+#This code is to generate a filepath variable to use in "Social Media Mental Health Notebook"
+
+filepath = "/Users/alexandragilbert/Desktop/python_projects/Social Media Mental Health/smmh_project/smmh.csv"
